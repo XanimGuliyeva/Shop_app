421a51227151329f86ef93d776fcf3a3cd4b473f
Xanim <57264503+XanimGuliyeva@users.noreply.github.com>
2023-01-25 22:44:04 +0400

    Add files via upload

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Basket = () => {
   return (
-    <div>
-      Basket
+    <div style={{padding:'20px'}}>
+      <h1>Shopping Basket</h1>  
+      <br/><br/>
+      <button>ChechOut</button>
+      <table style={{borderCollapse:"collapse",marginTop:'20px'}}>
+        <tr style={{width:'350px'}}>
+          <th style={{width:'200px',textAlign:'center'}}>Item</th>
+          <th style={{width:'200px',textAlign:'center'}}>Quantity</th>
+          <th style={{width:'200px',textAlign:'center'}}>Price</th>
+        </tr>
+        <tr>
+          <td style={{width:'200px',textAlign:'center'}}>Alfreds Futterkiste</td>
+          <td style={{width:'200px',textAlign:'center'}}>1 <button>+</button><button>-</button></td>
+          <td style={{width:'200px',textAlign:'center'}}>Germany</td>
+        </tr>
+      </table>
+      <br/>
+      <button>CLEAR</button>
+      <span>Total:</span>
     </div>
   )
 }
--- src/components/CategoryProduct.js
+++ src/components/CategoryProduct.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link,useNavigate} from 'react-router-dom'
+import {CartContext} from '../context/CartContext'
 
 const CategoryProduct = ({id,title,image,price})=> {
+
+  const cartContext = useContext(CartContext);
+  const addProduct = cartContext;
   const navigate = useNavigate();
+
   return (
     <div style={{display:'flex'}} key={id}>
         <img alt='img' style={{width:'150px',height:'150px'}} src={`/assest/${image}`}/>
         <div>
           <Link to={`/products/${id}`}>{title}</Link> - <span>&#8364;{price}</span> <br/>
            <br/>
-          <button onClick={()=>navigate(`products/${id}`)} style={{margin:'10px'}}>View Product</button> <br/>
-          <button style={{margin:'10px'}}>Add to basket</button> <br/>
+          <button onClick={()=>navigate(`/products/${id}`)} style={{margin:'10px'}}>View Product</button> <br/>
+          <button onClick={()=> addProduct({id,title,price})} style={{margin:'10px'}}>Add to basket</button> <br/>
         </div>
     </div>
   )
